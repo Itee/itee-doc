e47f181cfe962422fb58e95a64f8f05b34a13c9e
Tristan VALCKE <valcketristan@gmail.com>
2020-11-06 13:54:39 +0100

    docs(errorboundary): add class and methods descriptions

--- sources/components/ErrorBoundary.js
+++ sources/components/ErrorBoundary.js
@@ -2,23 +2,52 @@
 const React = require('react')
 const Card = require('react-bootstrap/Card')
 
+/**
+ * @class
+ * @classdesc An special component to wrap template error and display them to end user
+ * @augments React.Component
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ */
 class ErrorBoundary extends React.Component {
 
+    /**
+     * @constructor
+     * @param {Object} props - The component properties
+     */
     constructor ( props ) {
         super( props )
 
+        /**
+         * The local component state
+         * @type {Object}
+         */
         this.state = {
+            /**
+             * The current catched error
+             * @type {Object}
+             */
             error: null
         }
     }
 
+    /**
+     *
+     * @static
+     * @param error
+     * @returns {Object}
+     */
     static getDerivedStateFromError ( error ) {
         return {
             error
         }
     }
 
-    // Renderers
+    /**
+     * The main component render method
+     *
+     * @returns {JSX.Element}
+     */
     render () {
 
         if ( this.state.error ) {

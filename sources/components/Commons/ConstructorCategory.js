46d74d4b39aee9d108655692fc690abc744b20aa
Tristan VALCKE <valcketristan@gmail.com>
2021-07-27 10:30:16 +0200

    feat(constructorcategory): add new constructor category for standard display

---
+++ sources/components/Commons/ConstructorCategory.js
@@ -0,0 +1,21 @@
+const React    = require( 'react' )
+const Category = require( './Category' )
+const Constructor = require( '../Class/Constructor' )
+
+
+/**
+ * @class
+ * @classdesc The FunctionsCategory component allow to display preformated functions in Card style
+ * @augments React.Component
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ */
+class ConstructorCategory extends Category {
+
+    renderValue ( value ) {
+        return ( <Constructor { ...value }></Constructor> )
+    }
+
+}
+
+module.exports = ConstructorCategory
